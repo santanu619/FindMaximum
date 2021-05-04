2631ba8a732cd62286a84c4f93df9bb048574b1f
santanu619 <mohapatra.santanu123@gmail.com>
2021-05-04 17:00:58 +0530

    Added: Code for generic maximum

--- FindMaximumGenerics/Program.cs
+++ FindMaximumGenerics/Program.cs
@@ -10,18 +10,22 @@ namespace FindMaximumGenerics
     {
         static void Main(string[] args)
         {
-            int FirstValue = 12;
-            int SecondValue = 16;
-            int ThirdValue = 14;
-            float FirstFloatValue = 9.6f;
-            float SecondFloatValue = 4.8f;
-            float ThirdFloatValue = 2.3f;
-            string FirstPlace = "America";
-            string SecondPlace = "Australia";
-            string ThirdPlace = "India";
-            int maximum = FindMaxumum(FirstValue, SecondValue, ThirdValue);
-            float maximum1 = FindMaxumum1(FirstFloatValue, SecondFloatValue, ThirdFloatValue);
-            string maximum2 = FindMaximum2(FirstPlace, SecondPlace, ThirdPlace);
+            //int FirstValue = 12;
+            //int SecondValue = 16;
+            //int ThirdValue = 14;
+            //float FirstFloatValue = 9.6f;
+            //float SecondFloatValue = 4.8f;
+            //float ThirdFloatValue = 2.3f;
+            //string FirstPlace = "America";
+            //string SecondPlace = "Australia";
+            //string ThirdPlace = "India";
+           // int maximum = FindMaxumum(FirstValue, SecondValue, ThirdValue);
+            //float maximum1 = FindMaxumum1(FirstFloatValue, SecondFloatValue, ThirdFloatValue);
+            //string maximum2 = FindMaximum2(FirstPlace, SecondPlace, ThirdPlace);
+            string maximum = FindMaximum<int>(12, 16, 14);
+            string maximum1 = FindMaximum<float>(9.6f, 4.8f, 2.3f);
+            string maximum2 = FindMaximum<string>("America", "Australia", "India");
+
             Console.WriteLine(maximum);
             Console.WriteLine(maximum1);
             Console.WriteLine(maximum2);
@@ -60,5 +64,15 @@ namespace FindMaximumGenerics
             else throw new Exception("Values are same");
         }
 
+        static string FindMaximum<T>(T FirstPlace, T SecondPlace, T ThirdPlace) where T : IComparable<T>
+        {
+            if (FirstPlace.CompareTo(SecondPlace) > 0 && FirstPlace.CompareTo(ThirdPlace) > 0) 
+                return FirstPlace.ToString();
+            else if (SecondPlace.CompareTo(FirstPlace) > 0 && SecondPlace.CompareTo(ThirdPlace) > 0) 
+                return SecondPlace.ToString();
+            else if (ThirdPlace.CompareTo(FirstPlace) > 0 && ThirdPlace.CompareTo(SecondPlace) > 0) 
+                return ThirdPlace.ToString();
+            else throw new Exception("Values are same");
+        }
     }
 }
