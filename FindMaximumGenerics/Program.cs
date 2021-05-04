64926b8e2d340b60595bd5c237069bdd93e1eefc
santanu619 <mohapatra.santanu123@gmail.com>
2021-05-04 16:47:03 +0530

    Added: Code for Maximum String

--- FindMaximumGenerics/Program.cs
+++ FindMaximumGenerics/Program.cs
@@ -16,10 +16,15 @@ namespace FindMaximumGenerics
             float FirstFloatValue = 9.6f;
             float SecondFloatValue = 4.8f;
             float ThirdFloatValue = 2.3f;
+            string FirstPlace = "America";
+            string SecondPlace = "Australia";
+            string ThirdPlace = "India";
             int maximum = FindMaxumum(FirstValue, SecondValue, ThirdValue);
             float maximum1 = FindMaxumum1(FirstFloatValue, SecondFloatValue, ThirdFloatValue);
+            string maximum2 = FindMaximum2(FirstPlace, SecondPlace, ThirdPlace);
             Console.WriteLine(maximum);
             Console.WriteLine(maximum1);
+            Console.WriteLine(maximum2);
             Console.ReadKey();
 
         }
@@ -42,6 +47,16 @@ namespace FindMaximumGenerics
                 return SecondFloatValue;
             else if (ThirdFloatValue.CompareTo(FirstFloatValue) > 0 && ThirdFloatValue.CompareTo(SecondFloatValue) > 0)
                 return ThirdFloatValue;
+            else throw new Exception("Values are same");
+        }
+        static string FindMaximum2(string FirstPlace, string SecondPlace, string ThirdPlace)
+        {
+            if (FirstPlace.CompareTo(SecondPlace) > 0 && FirstPlace.CompareTo(ThirdPlace) > 0) 
+                return FirstPlace;
+            else if (SecondPlace.CompareTo(FirstPlace) > 0 && SecondPlace.CompareTo(ThirdPlace) > 0) 
+                return SecondPlace;
+            else if (ThirdPlace.CompareTo(FirstPlace) > 0 && ThirdPlace.CompareTo(SecondPlace) > 0) 
+                return ThirdPlace;
             else throw new Exception("Values are same");
         }
 
